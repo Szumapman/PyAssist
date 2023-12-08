2a133f9752774e2296d2c68383f8eb78451e5895
Szumapman <36560916+Szumapman@users.noreply.github.com>
2023-12-08 11:28:35 +0100

    Create a cli_pyassist file and add main(), error handling method, parsing method and main handling commands.

---
+++ cli_pyassist.py
@@ -0,0 +1,122 @@
+import sys
+import os
+from typing import Callable
+from utility.addressbook import AddresBook
+from utility.record import Record
+from utility.name import Name
+from utility.phone import Phone
+from utility.email import Email
+from utility.birthday import Birthday, FutureDateError
+
+#paths to files with data
+ADDRESBOOK_DATA_PATH = os.path.join(os.getcwd(), "data/addresbook.dat") # Because it's a simple program. The path is hard coded ;)
+#ścieżka do pliku z notatkami
+
+#objects storing data while the program is running
+ADDRESBOOK = AddresBook().load_addresbook(ADDRESBOOK_DATA_PATH)
+
+
+# function to handle with errors
+def error_handler(func: Callable):
+    def wrapper(*args):
+        while True:
+            try:
+                return func(*args)
+            # błędy i komunikaty przeniesione z pierwotnej wersji - do sprawdzenia / zmiany
+            except ValueError:
+                if func.__name__ == "add_name":
+                    print("The name field cannot be empty, try again.")
+                if func.__name__ == "add_phone":
+                    print("Invalid phone number, try again.")
+                if func.__name__ == "add_email":
+                    print("Invalid email, try again.")
+                if func.__name__ == "add_birthday":
+                    print("Invalid date format, try again.")
+                if func.__name__ == "import_from_csv":
+                    print("I can't import from this source. Check the file.")
+                    break
+            except FutureDateError:
+                print("You can't use a future date as a birthday, try again.")
+            except FileNotFoundError:
+                print("I can't find file to import data.")
+                break
+            except KeyboardInterrupt:
+                cli_pyassist_exit()
+    return wrapper
+
+
+# a function that parses user input commands
+def parse_command(user_input: str) -> (str, tuple):
+    """
+    Parse user input command
+
+    Args:
+        user_input (str): user input command
+    
+    Returns:
+        str: command
+        tuple: arguments
+    """
+    tokens = user_input.split()
+    command = tokens[0]
+    arguments = tokens[1:]
+    return command, tuple(arguments)
+
+
+
+# exit / close program
+def cli_pyassist_exit(*args):
+    ADDRESBOOK.save_addresbook(ADDRESBOOK_DATA_PATH)
+    # dodać zapisywanie notatek
+    print("Your data has been saved.") 
+    sys.exit("Good bye!")
+
+    
+# hendler for main menu
+def get_main_handler(command):
+    return MAIN_COMMANDS[command]
+
+   
+# dict for main menu handler
+MAIN_COMMANDS = {
+    "exit": cli_pyassist_exit,
+    # "add / new": create_record,
+    # "edit": edit_record,
+    # "delete / del": delete_record,
+    # "show": show_all,
+    # "search": search,
+    # "save": save_data,
+    # "export": export_to_csv,
+    # "import": import_from_csv,
+}
+
+
+def execute_commands(cmd, arguments):
+    """
+    Function to execute user commands
+
+    Args:
+        cmd (str): user command
+        arguments (tuple): arguments from user input
+
+    Returns:
+        func: function with arguments
+    """
+    if cmd not in MAIN_COMMANDS:
+        return f"Command {cmd} is not recognized"
+    cmd = MAIN_COMMANDS[cmd]
+    return cmd(*arguments)
+
+
+@error_handler
+def main():
+    print("Type command or help for command list.")
+    while True:
+        user_input = input(">>> ").strip().lower()
+        if user_input:
+            cmd, arguments = parse_command(user_input)
+            print(execute_commands(cmd, arguments))
+
+
+if __name__ == "__main__":
+    main()
