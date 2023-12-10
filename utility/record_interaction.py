4d257535c6748f2f9113222c9ccaa7015cefcc9c
Paweł Szumański <36560916+Szumapman@users.noreply.github.com>
2023-12-10 11:19:27 +0100

    Copied del_record to record_interaction.py

--- utility/record_interaction.py
+++ utility/record_interaction.py
@@ -116,4 +116,16 @@ def create_record(name):
         address = add_address()
     
     return Record(name, phones, emails, birthday, address)
+
+def del_record(addressbook, *args):
+    if not args:
+        name = input("Enter the name of the record you want to delete: ").strip().title()
+    else:
+        name = " ".join(args).strip().title()
+
+    if name in addressbook:
+        del addressbook[name]
+        return f"Record {name} deleted successfully."
+    else:
+        return f"Record {name} not found in the address book."
         
